91318b6c71a894c0670a297091b0fb16197c1b44
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-11-21 17:30:09 -0500

    Update predict.rb

--- predict.rb
+++ predict.rb
@@ -15,6 +15,20 @@ class Brain
     Vortex::Machine.node7; sleep(1)
     Vortex::Machine.node5; sleep(1)
 
+    # Write results of experiment to an XML file.
+    node1_section = File.read("Vortexination/Node1/_imaginedpath/outcomes/nuetral_outcome.txt")
+    node2_section = File.read("Vortexination/Node2/_imaginedpath/outcomes/nuetral_outcome.txt")
+    node4_section = File.read("Vortexination/Node4/_imaginedpath/outcomes/nuetral_outcome.txt")
+    node8_section = File.read("Vortexination/Node8/_imaginedpath/outcomes/nuetral_outcome.txt")
+    node7_section = File.read("Vortexination/Node7/_imaginedpath/outcomes/nuetral_outcome.txt")
+    node5_section = File.read("Vortexination/Node5/_imaginedpath/outcomes/nuetral_outcome.txt")
+
+    ## Formatting of XML document
+    open("_feed/feed.xml", "w") { |f|
+      f.puts "
+      "
+    }
+    
     state = "off"
   end
 
