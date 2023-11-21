2e76727461528b7e4389d218cd23ada0cfe9f948
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-11-21 17:08:05 -0500

    Create think.rb

---
+++ think.rb
@@ -0,0 +1,41 @@
+require_relative "Vortexination/vortex.rb"
+
+class Brain
+  attr_reader = @state
+  
+  def initialize
+    @state = off
+  end
+  
+  def on_behaviours
+    Vortex::Machine.node1; sleep(1)
+    Vortex::Machine.node2; sleep(1)
+    Vortex::Machine.node4; sleep(1)
+    Vortex::Machine.node8; sleep(1)
+    Vortex::Machine.node7; sleep(1)
+    Vortex::Machine.node5; sleep(1)
+
+    state = "off"
+  end
+
+  def off_behaviours
+    puts "Do you wish to exit? >> "; answer = gets.chomp
+
+    if    answer ==   "Y"; abort
+    elsif answer ==   "y"; abort
+    elsif answer == "Yes"; abort
+    elsif answer == "yes"; abort
+    else
+      puts "\nContinuing...\n\n"
+    end
+  end
+
+  def transition
+    case state
+    when "off"
+      on_behaviours
+    when "on"
+      off_behaviours
+    end
+  end
+end
